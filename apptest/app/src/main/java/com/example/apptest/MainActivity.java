97c6c269c6d99527802c3aa218050cef4b833c94
Jacob Ketterer <jketterer02@gmail.com>
2023-09-14 18:52:19 -0500

    trying to commit again

--- apptest/app/src/main/java/com/example/apptest/MainActivity.java
+++ apptest/app/src/main/java/com/example/apptest/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.apptest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+//commit test
 public class MainActivity extends AppCompatActivity {
 
     @Override

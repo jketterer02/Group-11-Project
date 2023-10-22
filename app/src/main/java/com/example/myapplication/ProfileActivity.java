82444888b2344db8e14c79241acf9ee898b91019
jketterer02 <jketterer02@gmail.com>
2023-10-22 15:12:24 -0500

    added back button which will go back to MainActivity

--- app/src/main/java/com/example/myapplication/ProfileActivity.java
+++ app/src/main/java/com/example/myapplication/ProfileActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -18,14 +18,19 @@ public class ProfileActivity extends AppCompatActivity {
     private TextView useremail, userID;
     FirebaseAuth auth;
     AuthCredential credential;
+    private Intent newsintent;
+    private ImageButton backtonews;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        newsintent = new Intent(this,MainActivity.class);
         useremail = findViewById(R.id.tv);
         userID = findViewById(R.id.tv2);
+        backtonews = findViewById(R.id.backtonewsbutton);
+        addListeners();
 
     }
     @Override
@@ -38,5 +43,17 @@ public class ProfileActivity extends AppCompatActivity {
         String UID = user.getUid();
         useremail.setText(email);
         userID.setText(UID);
+    }
+    private void addListeners()
+    {
+        backtonews.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(newsintent);
+                finish();
+            }
+        });
+
     }
 }

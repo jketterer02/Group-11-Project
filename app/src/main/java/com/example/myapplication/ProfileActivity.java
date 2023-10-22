48fa87c0db67154be22f945b4df7094060abe95d
jketterer02 <jketterer02@gmail.com>
2023-10-22 14:26:20 -0500

    added design for profile

--- app/src/main/java/com/example/myapplication/ProfileActivity.java
+++ app/src/main/java/com/example/myapplication/ProfileActivity.java
@@ -6,30 +6,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class ProfileActivity extends AppCompatActivity {
 
 
-    private TextView useremail;
+    private TextView useremail, userID;
     FirebaseAuth auth;
+    AuthCredential credential;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        auth = FirebaseAuth.getInstance();
         useremail = findViewById(R.id.tv);
+        userID = findViewById(R.id.tv2);
 
     }
     @Override
     protected void onStart()
     {
         super.onStart();
-        FirebaseUser user = auth.getCurrentUser();
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         String email = user.getEmail();
+        String UID = user.getUid();
         useremail.setText(email);
+        userID.setText(UID);
     }
 }

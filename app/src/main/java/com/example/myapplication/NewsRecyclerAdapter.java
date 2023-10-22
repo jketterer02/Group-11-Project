69dc4746cafbbc428fea6e1a0f16172da3bff7f9
johan <johanyf627@gmail.com>
2023-10-21 21:14:57 -0500

    created Java class put all the news information on the android screen.

---
+++ app/src/main/java/com/example/myapplication/NewsRecyclerAdapter.java
@@ -0,0 +1,62 @@
+package com.example.myapplication;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.kwabenaberko.newsapilib.models.Article;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class NewsRecyclerAdapter extends RecyclerView.Adapter<NewsRecyclerAdapter.NewsHolder>{
+
+    List<Article> articleList;
+    NewsRecyclerAdapter(List<Article> articleList){
+        this.articleList = articleList;
+    }
+    @NonNull
+    @Override
+    public NewsHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.news_recycler, parent, false);
+        return new NewsHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NewsHolder holder, int position) {
+        Article article = articleList.get(position);
+        holder.titleTextView.setText(article.getTitle());
+        holder.sourceTextView.setText(article.getSource().getName());
+        Picasso.get().load(article.getUrlToImage()).error(R.drawable.error_image_icon).placeholder(R.drawable.error_image_icon).into(holder.imageView);
+
+    }
+
+    void updateRecyclerView(List<Article> data){
+        articleList.clear();
+        articleList.addAll(data);
+    }
+
+    @Override
+    public int getItemCount() {
+        return articleList.size();
+    }
+
+    class NewsHolder extends RecyclerView.ViewHolder{
+
+        TextView titleTextView;
+        TextView sourceTextView;
+        ImageView imageView;
+        public NewsHolder(@NonNull View itemView) {
+            super(itemView);
+            titleTextView = itemView.findViewById(R.id.article_title);
+            sourceTextView = itemView.findViewById(R.id.article_source);
+            imageView = itemView.findViewById(R.id.article_image);
+
+        }
+    }
+}

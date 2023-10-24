8b7083de24f1973be5cfef2069c78d3cc26641a9
johan <johanyf627@gmail.com>
2023-10-24 02:52:50 -0500

    Added categories and search bar functionality. Also, added navigation bar and fragments but it is in work in progress.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,16 +2,28 @@
 
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.myapplication.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.progressindicator.LinearProgressIndicator;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -23,20 +35,43 @@ import com.kwabenaberko.newsapilib.models.response.ArticleResponse;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
     private Intent profileintent;
     RecyclerView recyclerView;
     List<Article> articleList = new ArrayList<>();
     NewsRecyclerAdapter adapter;
     LinearProgressIndicator progressIndicator;
+    Button btn1,btn2,btn3,btn4,btn5,btn6,btn7;
+    SearchView searchView;
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        //setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
 
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            if(item.getItemId() == R.id.home) {
+                //message to logcat to help debug and see if a button was clicked
+                Log.i("Mytag", "HOME");
+                replaceFragment(new HomeFragment());
+            } else if (item.getItemId() == R.id.search) {
+                Log.i("Mytag", "search");
+                replaceFragment(new SearchFragment());
+            } else if (item.getItemId() == R.id.account) {
+                Log.i("Mytag", "account");
+                replaceFragment(new AccountFragment());
+            }
+
+            return true;
+        });
 
 //        profileintent = new Intent(this,ProfileActivity.class);
 //        startActivity(profileintent);
@@ -46,12 +81,51 @@ public class MainActivity extends AppCompatActivity
         //  |  CODE ABOVE IS FOR TESTING PROFILE ACTIVITY |
         //  |                                             |
 
+
+
         recyclerView = findViewById(R.id.news_recyclerView);
         progressIndicator = findViewById(R.id.progress_line);
+        searchView = findViewById(R.id.search_view);
+        btn1 = findViewById(R.id.btn_1);
+        btn2 = findViewById(R.id.btn_2);
+        btn3 = findViewById(R.id.btn_3);
+        btn4 = findViewById(R.id.btn_4);
+        btn5 = findViewById(R.id.btn_5);
+        btn6 = findViewById(R.id.btn_6);
+        btn7 = findViewById(R.id.btn_7);
+
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+        btn5.setOnClickListener(this);
+        btn6.setOnClickListener(this);
+        btn7.setOnClickListener(this);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                getNews("GENERAL", query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
 
         setupRecyclerView();
-        getNews();
+        getNews("GENERAL", null);
 
+    }
+
+    private void replaceFragment(Fragment fragment) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
     @Override
     protected void onStart()
@@ -72,12 +146,12 @@ public class MainActivity extends AppCompatActivity
             progressIndicator.setVisibility(View.INVISIBLE);
     }
 
-    void getNews(){
+    void getNews(String category, String query){
         changeInProgress(true);
         NewsApiClient newsApiClient = new NewsApiClient("d70ae652c05b44ff97b52bd864da464c");
         //Gets all of the Top Head Lines News from the U.S.
         newsApiClient.getTopHeadlines(
-                new TopHeadlinesRequest.Builder().language("en").build(),
+                new TopHeadlinesRequest.Builder().language("en").category(category).q(query).build(),
                 new NewsApiClient.ArticlesResponseCallback() {
                     @Override
                     public void onSuccess(ArticleResponse response) {
@@ -96,5 +170,12 @@ public class MainActivity extends AppCompatActivity
                     }
                 }
         );
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button btn = (Button) v;
+        String category = btn.getText().toString();
+        getNews(category, null);
     }
 }

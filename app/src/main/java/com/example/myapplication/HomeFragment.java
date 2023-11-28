b9312521399da276a6f4077c191510b1515b4c20
johan <johanyf627@gmail.com>
2023-11-28 02:51:27 -0600

    Moved code from main activity to home fragment that handle the api request and setting up the RecyclerView.

--- app/src/main/java/com/example/myapplication/HomeFragment.java
+++ app/src/main/java/com/example/myapplication/HomeFragment.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.google.android.material.progressindicator.LinearProgressIndicator;
 import com.kwabenaberko.newsapilib.NewsApiClient;
@@ -21,10 +22,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
+    RecyclerView recyclerView;
+    List<Article> articleList = new ArrayList<>();
+    NewsRecyclerAdapter adapter;
+    LinearProgressIndicator progressIndicator;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 
     }
 
@@ -32,6 +38,61 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        recyclerView = (RecyclerView) view.findViewById(R.id.news_recyclerView);
+        progressIndicator = (LinearProgressIndicator) view.findViewById(R.id.progress_line);
+
+        setupRecyclerView();
+        getNews("GENERAL", null);
+        return view;
     }
+
+    void setupRecyclerView(){
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new NewsRecyclerAdapter(articleList);
+        recyclerView.setAdapter(adapter);
+    }
+
+    void changeInProgress(boolean showChange){
+        if(showChange)
+            progressIndicator.setVisibility(View.VISIBLE);
+        else
+            progressIndicator.setVisibility(View.INVISIBLE);
+    }
+
+    void getNews(String category, String query){
+        changeInProgress(true);
+        NewsApiClient newsApiClient = new NewsApiClient("d70ae652c05b44ff97b52bd864da464c");
+        //Gets all of the Top Head Lines News from the U.S.
+        newsApiClient.getTopHeadlines(
+                new TopHeadlinesRequest.Builder().language("en").category(category).q(query).build(),
+                new NewsApiClient.ArticlesResponseCallback() {
+                    @Override
+                    public void onSuccess(ArticleResponse response) {
+
+                        getActivity().runOnUiThread(()->{
+                            changeInProgress(false);
+                            articleList = response.getArticles();
+                            adapter.updateRecyclerView(articleList);
+                            adapter.notifyDataSetChanged();
+                        });
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        Log.i("Failure!", throwable.getMessage());
+                    }
+                }
+        );
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button btn = (Button) v;
+        String category = btn.getText().toString();
+        getNews(category, null);
+    }
+
+
 }

55c7b5e20a933f77ddca25b152e0c75c6040d268
jketterer02 <jketterer02@gmail.com>
2023-11-29 13:39:30 -0600

    started Account UI

--- app/src/main/java/com/example/myapplication/AccountFragment.java
+++ app/src/main/java/com/example/myapplication/AccountFragment.java
@@ -7,18 +7,34 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class AccountFragment extends Fragment {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    private TextView useremail, userID;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_account, container, false);
+    public void onCreate(Bundle savedInstanceState) {super.onCreate(savedInstanceState);}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View view =  inflater.inflate(R.layout.fragment_account, container, false);
+
+        //gets info of current User loggged in
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        //creates variables to edit the email and UID textviews
+        useremail = view.findViewById(R.id.Email);
+        userID = view.findViewById(R.id.UID);
+        //sets the textviews to the user's current email and UID
+        useremail.setText(user.getEmail());
+        userID.setText(user.getUid());
+
+        return view;
     }
 }

ed1ecab431e77b212e6e5fb330a46199e0112893
Jacob Ketterer <jketterer02@gmail.com>
2023-11-30 11:24:18 -0600

    removed unused imports

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -10,10 +10,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.AuthResult;
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,50 +1,20 @@
-// If we have errors enable virtualization
-
 package com.example.myapplication;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.viewpager2.widget.ViewPager2;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import com.example.myapplication.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.progressindicator.LinearProgressIndicator;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.kwabenaberko.newsapilib.NewsApiClient;
-import com.kwabenaberko.newsapilib.models.Article;
-import com.kwabenaberko.newsapilib.models.request.TopHeadlinesRequest;
-import com.kwabenaberko.newsapilib.models.response.ArticleResponse;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity
 {
-    //DEPRECIATED
-    //private Intent profileintent;
-    //Button btn1,btn2,btn3,btn4,btn5,btn6,btn7;
-    //List<Article> articleList = new ArrayList<>();
-    //RecyclerView recyclerView;
-    //NewsRecyclerAdapter adapter;
-    //LinearProgressIndicator progressIndicator;
-    //SearchView searchView;
 
     ActivityMainBinding binding;
 
@@ -53,7 +23,6 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
-        //setContentView(R.layout.activity_main);
         setContentView(binding.getRoot());
         replaceFragment(new HomeFragment());
 
@@ -74,54 +43,6 @@ public class MainActivity extends AppCompatActivity
             return true;
         });
 
-//        profileintent = new Intent(this,ProfileActivity.class);
-//        startActivity(profileintent);
-//        finish();
-
-        //  ^                                             ^
-        //  |  CODE ABOVE IS FOR TESTING PROFILE ACTIVITY |
-        //  |                                             |
-
-
-        /*
-        recyclerView = findViewById(R.id.news_recyclerView);
-        progressIndicator = findViewById(R.id.progress_line);
-
-        searchView = findViewById(R.id.search_view);
-        btn1 = findViewById(R.id.btn_1);
-        btn2 = findViewById(R.id.btn_2);
-        btn3 = findViewById(R.id.btn_3);
-        btn4 = findViewById(R.id.btn_4);
-        btn5 = findViewById(R.id.btn_5);
-        btn6 = findViewById(R.id.btn_6);
-        btn7 = findViewById(R.id.btn_7);
-
-        btn1.setOnClickListener(this);
-        btn2.setOnClickListener(this);
-        btn3.setOnClickListener(this);
-        btn4.setOnClickListener(this);
-        btn5.setOnClickListener(this);
-        btn6.setOnClickListener(this);
-        btn7.setOnClickListener(this);
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                getNews("GENERAL", query);
-                return true;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                return false;
-            }
-        });
-
-        setupRecyclerView();
-        getNews("GENERAL", null);
-
-         */
-
     }
 
     private void replaceFragment(Fragment fragment) {
@@ -137,65 +58,4 @@ public class MainActivity extends AppCompatActivity
         super.onStart();
     }
 
-    //Moved it to HomeFragment.java
-    /*
-    void setupRecyclerView(){
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new NewsRecyclerAdapter(articleList);
-        recyclerView.setAdapter(adapter);
-    }
-     */
-
-    //Moved it to HomeFragment.java
-    /*
-    void changeInProgress(boolean showChange){
-        if(showChange)
-            progressIndicator.setVisibility(View.VISIBLE);
-        else
-            progressIndicator.setVisibility(View.INVISIBLE);
-    }
-
-     */
-
-    //Moved it to HomeFragment.java
-    //This also has a search query so maybe move it to the SearchFragment.java
-    // Might have to remove the String query if moving it to the HomeFragment.java
-    /*
-    void getNews(String category, String query){
-        changeInProgress(true);
-        NewsApiClient newsApiClient = new NewsApiClient("d70ae652c05b44ff97b52bd864da464c");
-        //Gets all of the Top Head Lines News from the U.S.
-        newsApiClient.getTopHeadlines(
-                new TopHeadlinesRequest.Builder().language("en").category(category).q(query).build(),
-                new NewsApiClient.ArticlesResponseCallback() {
-                    @Override
-                    public void onSuccess(ArticleResponse response) {
-
-                        runOnUiThread(()->{
-                            changeInProgress(false);
-                            articleList = response.getArticles();
-                            adapter.updateRecyclerView(articleList);
-                            adapter.notifyDataSetChanged();
-                        });
-                    }
-
-                    @Override
-                    public void onFailure(Throwable throwable) {
-                        Log.i("Failure!", throwable.getMessage());
-                    }
-                }
-        );
-    }
-
-     */
-    /*
-    //Moved it to HomeFragment.java
-    @Override
-    public void onClick(View v) {
-        Button btn = (Button) v;
-        String category = btn.getText().toString();
-        getNews(category, null);
-    }
-
-     */
 }

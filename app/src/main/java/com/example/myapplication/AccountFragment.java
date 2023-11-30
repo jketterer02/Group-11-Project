b8f59a4e651b3ae97a6cb12630a360f7ebe56d36
jketterer02 <jketterer02@gmail.com>
2023-11-29 20:14:49 -0600

    changing pfp's added, will add firebase storage later

--- app/src/main/java/com/example/myapplication/AccountFragment.java
+++ app/src/main/java/com/example/myapplication/AccountFragment.java
@@ -1,12 +1,29 @@
 package com.example.myapplication;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import androidx.activity.result.ActivityResultLauncher;
+
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult;
+
+import androidx.activity.result.ActivityResult;
+
+import androidx.activity.result.ActivityResultCallback;
+
+import androidx.activity.result.contract.ActivityResultContracts;
+
+
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -16,6 +33,8 @@ import com.google.firebase.auth.FirebaseUser;
 public class AccountFragment extends Fragment {
 
     private TextView useremail, userID;
+    private ImageView profilepicture;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {super.onCreate(savedInstanceState);}
@@ -25,15 +44,42 @@ public class AccountFragment extends Fragment {
     {
         View view =  inflater.inflate(R.layout.fragment_account, container, false);
 
-        //gets info of current User loggged in
+        //gets info of current User logged in
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        //creates variables to edit the email and UID textviews
+        //puts the text views and imageview into variables for later use
         useremail = view.findViewById(R.id.Email);
         userID = view.findViewById(R.id.UID);
+        profilepicture = view.findViewById(R.id.pfp);
+
         //sets the textviews to the user's current email and UID
         useremail.setText(user.getEmail());
         userID.setText(user.getUid());
 
+        //adds the click listeners
+        addListeners();
+
         return view;
+    }
+    private void addListeners()
+    {
+        profilepicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //profilepicture.setVisibility(View.INVISIBLE);
+                //test code
+                Intent intent = new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent,3);
+
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode== Activity.RESULT_OK && data!=null){
+            Uri selectedImage = data.getData();
+            profilepicture.setImageURI(selectedImage);
+        }
     }
 }

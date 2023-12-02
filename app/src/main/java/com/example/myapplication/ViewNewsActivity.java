dd372817dfa2689f566796b4a706c6704e65107d
jketterer02 <jketterer02@gmail.com>
2023-12-02 16:04:42 -0600

    Back button on newsview

--- app/src/main/java/com/example/myapplication/ViewNewsActivity.java
+++ app/src/main/java/com/example/myapplication/ViewNewsActivity.java
@@ -1,19 +1,32 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.example.myapplication.databinding.ActivityMainBinding;
 
 public class ViewNewsActivity extends AppCompatActivity {
 
     WebView webView;
+    ImageView backbutton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_news);
+
+        backbutton = findViewById(R.id.back_to_news);
 
         String URL = getIntent().getStringExtra("url");
         webView = findViewById(R.id.web_view);
@@ -22,14 +35,26 @@ public class ViewNewsActivity extends AppCompatActivity {
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl(URL);
 
+        addListeners();
+
     }
 
     @Override
-    public void onBackPressed() {
-        if(webView.canGoBack()){
-            webView.goBack();
-        } else {
-            super.onBackPressed();
-        }
+    public void onBackPressed()
+    {
+        if(webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
+    }
+
+    private void addListeners()
+    {
+
+        //Listener for clicking back button
+        backbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {onBackPressed();}
+        });
+
+
     }
 }

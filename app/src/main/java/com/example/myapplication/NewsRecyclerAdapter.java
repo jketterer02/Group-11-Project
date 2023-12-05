8513f58fe9e9f64665c20d074d50223116842e0b
Jacob Ketterer <jketterer02@gmail.com>
2023-12-04 18:56:24 -0600

    fixed bugs with likecount

--- app/src/main/java/com/example/myapplication/NewsRecyclerAdapter.java
+++ app/src/main/java/com/example/myapplication/NewsRecyclerAdapter.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.kwabenaberko.newsapilib.models.Article;
 import com.squareup.picasso.Picasso;
 
@@ -24,30 +25,42 @@ public class NewsRecyclerAdapter extends RecyclerView.Adapter<NewsRecyclerAdapte
 
     private List<Article> articleList;
 
-    //likeCounts are stored in a hashmap
+    //likecounts are stored in a hashmap
     private Map<String, Integer> likecounts = new HashMap<>();
+    //likedarticles are stored in a hashmap with boolean values
+    private Map<String, Boolean> likedarticles = new HashMap<>();
+
+    FirebaseAuth auth = FirebaseAuth.getInstance();
 
     //local database to store like counts
     //works between users
-    private SharedPreferences sharedPreferences;
+    private SharedPreferences sharedPreferenceslikecount;
+    private SharedPreferences sharedPreferenceslikedarticles;
 
     NewsRecyclerAdapter(List<Article> articleList, Context context) {
         this.articleList = articleList;
         //creates the sharedPreferences database
-        this.sharedPreferences = context.getSharedPreferences("likeCounts", Context.MODE_PRIVATE);
-        initializeLikeCounts();
+        this.sharedPreferenceslikecount = context.getSharedPreferences("likecounts", Context.MODE_PRIVATE);
+        this.sharedPreferenceslikedarticles = context.getSharedPreferences("likedarticles"+ auth.getUid(), Context.MODE_PRIVATE);
+        initializelikecounts();
     }
 
-    //Updates the like counts to what they should be when loaded into the newsview
-    private void initializeLikeCounts() {
+    private void initializelikecounts()
+    {
+        //clear like count and liked articles
         likecounts.clear();
+        likedarticles.clear();
         for (int i=0;i<articleList.size();i++)
         {
             Article article = articleList.get(i);
-            // Retrieve the like count from SharedPreferences, defaults to 0 if not found
-            int storedLikeCount = sharedPreferences.getInt(article.getUrl(), 0);
-            // Puts the value from Shared Preferences into the likeCounts HashMap
+            // Gets like count from sharedpreferences, defaults to 0 if not found
+            int storedLikeCount = sharedPreferenceslikecount.getInt(article.getUrl(), 0);
+            // Puts the value from sharedpreferences into the likecounts hashmap
             likecounts.put(article.getUrl(), storedLikeCount);
+            // Check if article was liked before and update likedarticles
+            boolean isLiked = sharedPreferenceslikedarticles.getBoolean(article.getUrl() + "_liked", false);
+            //Put boolean value into the likedarticles hashmap
+            likedarticles.put(article.getUrl(), isLiked);
         }
     }
 
@@ -65,57 +78,55 @@ public class NewsRecyclerAdapter extends RecyclerView.Adapter<NewsRecyclerAdapte
         holder.sourceTextView.setText(article.getSource().getName());
         Picasso.get().load(article.getUrlToImage()).error(R.drawable.error_image_icon).placeholder(R.drawable.error_image_icon).into(holder.imageView);
 
+        //Puts you into ViewNews Activity
         holder.itemView.setOnClickListener((view -> {
             Intent intent = new Intent(view.getContext(), ViewNewsActivity.class);
             intent.putExtra("url", article.getUrl());
             view.getContext().startActivity(intent);
         }));
 
-        // Sets the like count for the current article from the HashMap
+        //sets the like count and like button state for the current article
         holder.like_count.setText(String.valueOf(likecounts.get(article.getUrl())));
+        //sets the liked boolean value of the current article
+        holder.liked = likedarticles.get(article.getUrl());
+        //if the article was liked, set the imageview accordingly to boolean value
+        if (holder.liked) holder.likebutton.setImageResource(R.drawable.liked_button);
+        else holder.likebutton.setImageResource(R.drawable.like_button);
 
-        holder.likebutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Get the current like count for the article from the HashMap
-                int currentLikeCount = likecounts.get(article.getUrl());
-                //if the holder was liked, unlike the button and decrease the like count, else like the button and increase the like count
-                if (holder.liked)
-                {
-                    holder.likebutton.setImageResource(R.drawable.like_button);
-                    likecounts.put(article.getUrl(), currentLikeCount - 1);
-                }
-                else
-                {
-                    holder.likebutton.setImageResource(R.drawable.liked_button);
-                    likecounts.put(article.getUrl(), currentLikeCount + 1);
-                }
-                //Update the like count TextView
-                holder.like_count.setText(String.valueOf(likecounts.get(article.getUrl())));
-                //flip the value of isliked
-                holder.liked = !holder.liked;
-                //update the database of likes
-                sharedPreferences.edit().putInt(article.getUrl(), likecounts.get(article.getUrl())).apply();
+        holder.likebutton.setOnClickListener(v -> {
+            int currentLikeCount = likecounts.get(article.getUrl());
+            boolean isLiked = likedarticles.get(article.getUrl());
+            //updates likecount
+            if (isLiked)
+            {
+                holder.likebutton.setImageResource(R.drawable.like_button);
+                likecounts.put(article.getUrl(), currentLikeCount - 1);
+            } else
+            {
+                holder.likebutton.setImageResource(R.drawable.liked_button);
+                likecounts.put(article.getUrl(), currentLikeCount + 1);
             }
+            //Update the like count TextView
+            holder.like_count.setText(String.valueOf(likecounts.get(article.getUrl())));
+            //flip the value of the likedarticles value, changing key
+            likedarticles.put(article.getUrl(), !isLiked);
+            //updates the database with likecounts and liked boolean value
+            sharedPreferenceslikecount.edit().putInt(article.getUrl(), likecounts.get(article.getUrl())).apply();
+            sharedPreferenceslikedarticles.edit().putBoolean(article.getUrl() + "_liked", !isLiked ).apply();
         });
     }
 
-    //updates the list of articles
     void updateRecyclerView(List<Article> data)
     {
         articleList.clear();
         articleList.addAll(data);
-        initializeLikeCounts();
+        initializelikecounts();
     }
 
     @Override
-    public int getItemCount()
-    {
-        return articleList.size();
-    }
+    public int getItemCount() {return articleList.size();}
 
     static class NewsHolder extends RecyclerView.ViewHolder {
-
         TextView titleTextView;
         TextView sourceTextView;
         ImageView imageView;
@@ -131,15 +142,12 @@ public class NewsRecyclerAdapter extends RecyclerView.Adapter<NewsRecyclerAdapte
             likebutton = itemView.findViewById(R.id.like_button);
             like_count = itemView.findViewById(R.id.like_count);
 
-            likebutton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v)
-                {
-                    if (liked) likebutton.setImageResource(R.drawable.like_button);
-                    else likebutton.setImageResource(R.drawable.liked_button);
-                    liked = !liked;
-                }
+            likebutton.setOnClickListener(v -> {
+                if (liked) likebutton.setImageResource(R.drawable.like_button);
+                else likebutton.setImageResource(R.drawable.liked_button);
+                liked = !liked;
             });
         }
     }
 }
+

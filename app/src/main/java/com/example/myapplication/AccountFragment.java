0835361c6fbb400bb2c6ee7fae8af021a4e0b9ab
Jacob Ketterer <jketterer02@gmail.com>
2023-12-06 14:09:43 -0600

    started on profile likesview

--- app/src/main/java/com/example/myapplication/AccountFragment.java
+++ app/src/main/java/com/example/myapplication/AccountFragment.java
@@ -1,11 +1,16 @@
 package com.example.myapplication;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.content.SharedPreferences;
 
 //import androidx.activity.result.ActivityResultLauncher;
 //
@@ -19,6 +24,7 @@ import androidx.fragment.app.Fragment;
 
 
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,18 +35,32 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 //import com.google.android.gms.tasks.OnFailureListener;
 //import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.progressindicator.LinearProgressIndicator;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.kwabenaberko.newsapilib.NewsApiClient;
+import com.kwabenaberko.newsapilib.models.Article;
+import com.kwabenaberko.newsapilib.models.request.EverythingRequest;
+import com.kwabenaberko.newsapilib.models.response.ArticleResponse;
+
+import java.util.ArrayList;
+import java.util.List;
 //import com.google.firebase.storage.UploadTask;
 
 
 public class AccountFragment extends Fragment {
 
     private TextView useremail, userID;
-    private ImageView profilepicture, signout_button;
+    private ImageView profilepicture, signout_button, likes;
     private Uri selectedImage;
+
+    RecyclerView recyclerView;
+    List<Article> articleList = new ArrayList<>();
+    NewsRecyclerAdapter adapter;
+    LinearProgressIndicator progressIndicator;
+
 
 
     @Override
@@ -58,10 +78,15 @@ public class AccountFragment extends Fragment {
         userID = view.findViewById(R.id.UID);
         profilepicture = view.findViewById(R.id.pfp);
         signout_button = view.findViewById(R.id.sign_out_button);
+        likes = view.findViewById(R.id.likelist);
+
 
         //sets the textviews to the user's current email and UID
         useremail.setText(user.getEmail());
         userID.setText(user.getUid());
+
+        recyclerView = (RecyclerView) view.findViewById(R.id.news_recyclerView);
+        progressIndicator = (LinearProgressIndicator) view.findViewById(R.id.progress_line);
 
         //gets the current pfp storage reference, and downloads it from firebase
         //if this is successful, it puts the result into profilepicture
@@ -69,8 +94,11 @@ public class AccountFragment extends Fragment {
             if(task.isSuccessful()) Glide.with(requireContext()).load(task.getResult()).into(profilepicture);
         });
 
+        progressIndicator.setVisibility(View.INVISIBLE);
+        recyclerView.setVisibility(View.INVISIBLE);
         //adds the click listeners
         addListeners();
+
 
         return view;
     }
@@ -86,6 +114,12 @@ public class AccountFragment extends Fragment {
         signout_button.setOnClickListener(v -> {
             Intent intent = new Intent(getActivity(), LoginActivity.class);
             startActivity(intent);
+        });
+
+        likes.setOnClickListener(v -> {
+            setupRecyclerView();
+            getNews("GENERAL", null);
+            recyclerView.setVisibility(View.VISIBLE);
         });
     }
     //this code is depreciated but it's the only thing I can find that works
@@ -110,5 +144,48 @@ public class AccountFragment extends Fragment {
     {
         String uid = FirebaseAuth.getInstance().getUid();
         return FirebaseStorage.getInstance().getReference().child("profile_pic").child(uid);
+    }
+
+    void setupRecyclerView(){
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new NewsRecyclerAdapter(articleList, requireContext());
+        recyclerView.setAdapter(adapter);
+    }
+
+    void changeInProgress(boolean showChange){
+        if(showChange)
+            progressIndicator.setVisibility(View.VISIBLE);
+        else
+            progressIndicator.setVisibility(View.INVISIBLE);
+    }
+
+    void getNews(String category, String query){
+        changeInProgress(true);
+        NewsApiClient newsApiClient = new NewsApiClient("d70ae652c05b44ff97b52bd864da464c");
+        newsApiClient.getEverything(
+                new EverythingRequest.Builder().language("en").q("general").from("2023-11-29").sortBy("popularity").build(),
+
+                new NewsApiClient.ArticlesResponseCallback() {
+                    @Override
+                    public void onSuccess(ArticleResponse response) {
+
+                        getActivity().runOnUiThread(()->{
+                            changeInProgress(false);
+                            articleList = response.getArticles();
+                            adapter.updateRecyclerView(articleList);
+
+                            adapter.notifyDataSetChanged();
+                        });
+
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        Log.i("Failure!", throwable.getMessage());
+                    }
+                }
+
+        );
+
     }
 }
